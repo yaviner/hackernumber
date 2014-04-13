cbe901a190a82b95697507ad20caf459a5d49659
Jromer94 <Jromer94@gmail.com>
2014-04-12 23:55:01 -0400

    finished recursive dfs

--- main.py
+++ main.py
@@ -1,35 +1,60 @@
 from flask import Flask, request, render_template, Markup, json
 import requests
+from sets import Set
 
 app = Flask(__name__)
 
-def match_users(user, target):
+def match_users(user, target, user_set, repo_set, level, degree_list):
     payload = {'type': 'all', 'access_token': '7b5e948bbb33a71fa7842b970b835a6717b28050'}
     payload2 = {'access_token': '7b5e948bbb33a71fa7842b970b835a6717b28050'}
+    
     r = requests.get('https://api.github.com/users/' + user + '/repos', params=payload)
+    if not r.status_code == requests.codes.ok:
+        print r.status_code
+        return False
+
     user_repos_raw = r.json()
+
     user_repos = {}
 
     for repos in user_repos_raw:
-        user_repos[repos['full_name']] = repos['contributors_url']
+        if repos['full_name'] not in repo_set:
+            user_repos[repos['full_name']] = repos['contributors_url']
+            repo_set.add(repos['full_name'])
 
-    users = {}
+
+    users = Set([])
 
     for repos in user_repos.values(): 
-        print repos
-        q = requests.get(repos, params=payload2)
-        users_raw = q.json()
+        try:
+            print repos
+            q = requests.get(repos, params=payload2)
+            users_raw = q.json()
+            if not r.status_code == requests.codes.ok:
+                print r.status_code
+                continue
+        except Exception as inst:
+            continue;
         for person in users_raw:
-            users[person['login']] = 'true' 
-    if users.has_key(target):
-        return target 
+            if person['login'] not in user_set:
+                users.add(person['login']) 
+                user_set.add(person['login'])
+    if target in users:
+        degree_list.append(target)
+        return True
+    for person in users:
+       if  level < 3 and match_users(person, target, user_set, repo_set, level + 1, degree_list):
+           degree_list.append(person)
+           return True
 
-    return 'null'
+    return False
 
 
 @app.route('/')
 def hello_world():
-    names = match_users('jromer94', 'adispen')
+    end_set = []
+    names = match_users('jromer94', 'sagnew', Set([]), Set([]), 1, end_set)
+    print end_set
     return 'names'
 
 @app.route('/user/<username>')
